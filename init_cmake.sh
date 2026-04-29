fb2921e26074b34253b114b0e39a7bcafe0688cf
JoonHo Son <joonho.son@me.com>
2026-04-29 16:27:44 +0900

    chore: 초기화 스크립트 debug/release 인자 수신 하도록 수정

--- init_cmake.sh
+++ init_cmake.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+build_type=${1:-debug}
+
 VENDOR_DIR="./vendor"
 rm -rf build
 
-if [ -d "$VENDOR_DIR" ]; then
-    echo "Remove all subdirectories in vendor."
-    rm -rf "${VENDOR_DIR}/*"
-else
-    echo "Create vendor directory."
-    mkdir "$VENDOR_DIR"
-fi
+#if [ -d "$VENDOR_DIR" ]; then
+#    echo "Remove all subdirectories in vendor."
+#    rm -rf "${VENDOR_DIR}/*"
+#else
+#    echo "Create vendor directory."
+#    mkdir "$VENDOR_DIR"
+#fi
 
 # cp ./CMakeLists_vendor.txt "$VENDOR_DIR/CMakeLists.txt"
 
-echo "-----------------------------------------------------------------------------"
-echo "Start project[Debug] initializing..."
-echo "-----------------------------------------------------------------------------"
-cmake -S . -DCMAKE_BUILD_TYPE=Debug -B build/debug
-
-echo "-----------------------------------------------------------------------------"
-echo "Start project[Release] initializing..."
-echo "-----------------------------------------------------------------------------"
-cmake -S . -DCMAKE_BUILD_TYPE=Release -B build/release
+if [ "debug" == "$build_type" ]; then
+    echo "-----------------------------------------------------------------------------"
+    echo "Start project[Debug] initializing..."
+    echo "-----------------------------------------------------------------------------"
+    cmake -S . -DCMAKE_BUILD_TYPE=Debug -B build/debug
+else
+    echo "-----------------------------------------------------------------------------"
+    echo "Start project[Release] initializing..."
+    echo "-----------------------------------------------------------------------------"
+    cmake -S . -DCMAKE_BUILD_TYPE=Release -B build/release
+fi
